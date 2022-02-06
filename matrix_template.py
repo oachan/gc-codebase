bdc413ddbb8fb3d67e7169c470a9ba6978af7817
GuanCheng <a2961353@gmail.com>
2022-02-06 19:03:49 +0800

    Update matrix_template.py

--- matrix_template.py
+++ matrix_template.py
@@ -1,9 +1,21 @@
+#!/usr/bin/env python3
+
 def Basic():
     
-    def dfs():
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])): 
-                print(matrix[i][j])
+    def init():
+        visited = [[False for _ in arr[0]] for _ in arr]
+
+    def dfs(arr, i, j, visited):
+        if i < 0 or j < 0 or i >= len(arr) or j >= len(arr[0]):
+            return
+        if visited[i][j] == True:
+            return
+        visited[i][j] = True
+        # Do action here...
+        dfs(arr, i + 1, j, visited)
+        dfs(arr, i - 1, j, visited)
+        dfs(arr, i, j + 1, visited)
+        dfs(arr, i, j - 1, visited)
 
     def bfs():
         queue = []
