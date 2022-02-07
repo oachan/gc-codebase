04980bdd8f29a0a3dfba05adccc5fa1fdf9f592d
GuanCheng <a2961353@gmail.com>
2022-02-07 20:08:03 +0800

    Update sort_template.py

--- sort_template.py
+++ sort_template.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-def binary_search():
-    pass
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) / 2
+        if arr[mid] < target:
+            right = mid + 1
+        elif arr[mid] > target:
+            left = mid - 1
+        else:
+            arr[mid] == target:
+            return mid
+    return -1
