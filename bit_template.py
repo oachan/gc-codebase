9d297ca14cb96d860881dd9c92d84f5fdec78020
GuanCheng <a2961353@gmail.com>
2022-02-08 01:01:27 +0800

    Update bit_template.py

--- bit_template.py
+++ bit_template.py
@@ -3,3 +3,10 @@
 MAX_INT = 0x7FFFFFFF
 MIN_INT = 0x80000000
 MASK = 0x100000000
+
+def bitarray(arr):
+    bitarray = 1 << len(arr)
+    bitarray = bitarray | (1 << bit)  # on
+    bitarray = bitarray & ~(1 << bit)  # off
+    flag = bitarray & (1 << bit)
+    
