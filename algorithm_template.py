b919e025b189cd22e3997898c494c7fb2e9a9163
GuanCheng <a2961353@gmail.com>
2022-02-08 00:07:03 +0800

    Update algorithm_template.py

--- algorithm_template.py
+++ algorithm_template.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+def dynamic_programming_fibonacci(k):
+    dp = [0 for _ in range(k)]
+    dp[0], dp[1] = 1, 1
+    for i in range(2, k):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    return dp[-1]
+
 def sliding_window(arr):
     start = 0
     for end in range(len(arr)):
-        # Do something.
-        while is_(start):
-             # Do something.
+        # Do something at end.
+        while is_endfail_condiction(start):
+            # Do something at start.
             start += 1
