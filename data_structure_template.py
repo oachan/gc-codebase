7777ff1e9d36bff8469583ad7f3d202016909194
GuanCheng <a2961353@gmail.com>
2022-02-08 00:39:48 +0800

    Update data_structure_template.py

--- data_structure_template.py
+++ data_structure_template.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+def char_dictionary():
+    lower_dict = {chr(i): i - 96 for i in range(97, 123)}
+    ipper_dict = {chr(i): i - 64 for i in range(65, 91)}
+
 def heap(arr):
     import heapq
     heap = arr
