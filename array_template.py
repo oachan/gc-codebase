f0b499a0db85c9be247c4c2288787792836b8007
GuanCheng <a2961353@gmail.com>
2022-02-08 00:54:19 +0800

    Update and rename algorithm_template.py to array_template.py

--- array_template.py
+++ array_template.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
-def dynamic_programming_fibonacci(k):
-    dp = [0 for _ in range(k)]
-    dp[0], dp[1] = 1, 1
-    for i in range(2, k):
-        dp[i] = dp[i - 2] + dp[i - 1]
-    return dp[-1]
+def two_pointer(arr):
+    pass
 
 def sliding_window(arr):
     start = 0
@@ -14,3 +10,10 @@ def sliding_window(arr):
         while is_endfail_condiction():
             # Do something at start.
             start += 1
+
+def dynamic_programming_fibonacci(k):
+    dp = [0 for _ in range(k)]
+    dp[0], dp[1] = 1, 1
+    for i in range(2, k):
+        dp[i] = dp[i - 2] + dp[i - 1]
+    return dp[-1]
