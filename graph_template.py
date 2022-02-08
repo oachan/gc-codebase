30192f73a3e2582f18f37eee0a1f935b34a7e9f4
GuanCheng <a2961353@gmail.com>
2022-02-08 23:41:32 +0800

    Create graph_template.py

---
+++ graph_template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def basic():
+    class Graph:
+        def __init__(self):
+            self.val = None
+            self.neighbors = []
+def dfs():
+    pass
+
+def bfd():
+    pass
+
+def topological_sort():
+    pass
