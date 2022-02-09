0e153751d72d7a133b5f62a16414bf07b50c0f7e
GuanCheng <a2961353@gmail.com>
2022-02-09 20:53:22 +0800

    Update graph_template.py

--- graph_template.py
+++ graph_template.py
@@ -5,8 +5,13 @@ def basic():
         def __init__(self):
             self.val = None
             self.neighbors = []
-def dfs():
-    pass
+
+def dfs(node, visited):
+    if node is None:
+        return
+    visited[node.val] = True
+    for next_node in node.neighbors:
+        dfs(next_node, visited)
 
 def bfd():
     pass
