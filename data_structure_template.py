51d9396c8ef03294d257ae1e407ff4f236a9f8e9
GuanCheng <a2961353@gmail.com>
2022-02-11 00:02:33 +0800

    Update data_structure_template.py

--- data_structure_template.py
+++ data_structure_template.py
@@ -2,7 +2,7 @@
 
 def char_dictionary():
     lower_dict = {chr(i): i - 96 for i in range(97, 123)}
-    ipper_dict = {chr(i): i - 64 for i in range(65, 91)}
+    upper_dict = {chr(i): i - 64 for i in range(65, 91)}
 
 def heap(arr):
     import heapq
