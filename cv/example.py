b962630099ce9a23eb412381d50599b9e017c57c
GuanCheng <a2961353@gmail.com>
2022-12-29 22:58:52 +0800

    Update example.py

--- cv/example.py
+++ cv/example.py
@@ -26,6 +26,7 @@ def morphology():
     img = cv2.erode(img, kernel)   # 侵蝕
     img = cv2.dilate(img, kernel)  # 擴張
 
+
 def morphology():
     opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=2)   # 開運算
     closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=2)  # 閉運算
@@ -33,13 +34,17 @@ def morphology():
     tophat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
     blackhat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, kernel)
 
+
 #  Structural Analysis and Shape Descriptors
 def contours():
-    cv2.findContours(img)
-    drawContours()
-    moments()
-    contourArea()
-    arcLength()
-    boundingRect()
-    convexHull()
-    fitEllipse()
+    img = cv2.imread("image.jpg")
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    thr_min, thr_max= 200, 255
+    _, threshold = cv2.threshold(img, thr_min, thr_max, 0)
+    contours, _ = cv2.findContours(threshold, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(img, contours=contours, contourIdx=-1, color=(0, 255, 255), thickness=2)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)  # 面積
